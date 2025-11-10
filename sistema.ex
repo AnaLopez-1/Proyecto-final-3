355d5b535826b169b57b62fbaac963f1ca7fa55d
Maritza245 <calderonzapatan@gmail.com>
2025-11-10 17:56:21 -0500

    Crear entidades (proyecto)

--- sistema.ex
+++ sistema.ex
@@ -6,9 +6,13 @@ defmodule Sistema do
   def crear_participante(id, nombre, correo, afinidad) do
     Modelos.Participante.crear(id, nombre, correo, afinidad)
   end
-def crear_equipo(nombre, tema) do
+  def crear_equipo(nombre, tema) do
     Modelos.Equipo.crear(nombre, tema)
   end
  def crear_mentor(id, nombre, especialidad) do
     Modelos.Mentor.crear(id, nombre, especialidad)
   end
+  def crear_proyecto(nombre, descripcion, categoria) do
+    Modelos.Proyecto.crear(nombre, descripcion, categoria)
+  end
+  
