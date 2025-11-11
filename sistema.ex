d3c00fbbd29cb309628eb8bcf9eec81a867d8731
Maritza245 <calderonzapatan@gmail.com>
2025-11-10 20:09:52 -0500

    Guardar datos

--- sistema.ex
+++ sistema.ex
@@ -6,13 +6,20 @@ defmodule Sistema do
   def crear_participante(id, nombre, correo, afinidad) do
     Modelos.Participante.crear(id, nombre, correo, afinidad)
   end
+
   def crear_equipo(nombre, tema) do
     Modelos.Equipo.crear(nombre, tema)
   end
- def crear_mentor(id, nombre, especialidad) do
+
+  def crear_mentor(id, nombre, especialidad) do
     Modelos.Mentor.crear(id, nombre, especialidad)
   end
+
   def crear_proyecto(nombre, descripcion, categoria) do
     Modelos.Proyecto.crear(nombre, descripcion, categoria)
+  end
+  # Guardar en archivos globales
+  def guardar_datos(tipo, dato) do
+    Persistencia.guardar(tipo, dato)
   end
   
